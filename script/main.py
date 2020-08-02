6971be3aeca4d2d022bdcb8300306306b9340c17
Anna Langner <annalangner06@gmail.com>
2020-08-02 22:00:14 +0200

    Printing the operation name

--- script/main.py
+++ script/main.py
@@ -6,7 +6,7 @@ def init_argparser():
   """Fetch arguments from command line"""
 
   parser = argparse.ArgumentParser()
-  subparsers = parser.add_subparsers(help='sub-command help')
+  subparsers = parser.add_subparsers(help='sub-command help', dest='operation')
   # create the parser for the init command
   parser_init = subparsers.add_parser('init', help='init command')
   # add parameter
@@ -28,13 +28,15 @@ def init_db():
 def main():
   results = []
   args = init_argparser()
-  if args == 'init':
+  if args.operation == 'init':
     results = init_db()
-  elif args == 'percentage':
+  elif args.operation == 'percentage':
     results = percentage()
 
-  for r in results:
-    print(r)  
+  # for r in results:
+  #   print(r)  
+
+  print(args.operation)
 
 
 main()
