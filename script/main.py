1c72158c4d8aebdb4a576f723a7e94d5360fd9c0
Anna Langner <annalangner06@gmail.com>
2020-08-02 14:12:18 +0200

    Changing the concept of parsing arguments

--- script/main.py
+++ script/main.py
@@ -4,23 +4,27 @@ import requests
 import re
 
 
-def fetch_arguments():
+def init_argparser():
   """Fetch arguments from command line"""
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('--command', help='entry command')
-  parser.add_argument('--parameter', help='entry parameter: female, male, number or date')
-  args = parser.parse_args()  
-  return (args.command, args.parameter)
+  subparsers = parser.add_subparsers(help='sub-command help')
+  # create the parser for the first command
+  parser_command = subparsers.add_parser('command', help='entry command')
+  # add parameter
+  parser_command.add_argument('--parameter', help='Entry parameter')
+  args = parser.parse_args()
+  return args
+
 
 def init_db():
+  init_argparser()
   '''Initializing the database'''
   
   print('Nothing is implemented')
 
 
 def main():
-  fetch_arguments()
   init_db()
 
 
