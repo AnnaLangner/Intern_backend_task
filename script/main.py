4303ed136af8ea5079dc83f88d23e46fba33e992
Anna Langner <annalangner06@gmail.com>
2020-08-02 15:44:16 +0200

    Added new subparser

--- script/main.py
+++ script/main.py
@@ -1,30 +1,33 @@
 #A script that takes command and parameter as an argument
 import argparse
-import requests
-import re
 
 
 def init_argparser():
   """Fetch arguments from command line"""
 
   parser = argparse.ArgumentParser()
-  subparsers = parser.add_subparsers(help='sub-command help')
-  # create the parser for the first command
-  parser_command = subparsers.add_parser('command', help='entry command')
+  subparsers = parser.add_subparsers(help='sub-command help', dest='subparser_name')
+  # create the parser for the init command
+  parser_init = subparsers.add_parser('init', help='init command')
   # add parameter
-  parser_command.add_argument('--parameter', help='Entry parameter')
+  parser_init.add_argument('--file', help='Path to the initial file')
+  # create the parser for the percentage command
+  parser_percentage = subparsers.add_parser('percentage', help='Enter command')
+  # add parameter
+  parser_percentage.add_argument('--female', help='Enter gender')
+  parser_percentage.add_argument('--male', help='Enter gender')
   args = parser.parse_args()
   return args
 
 
-def init_db():
-  init_argparser()
+def init_db():  
   '''Initializing the database'''
-  
+
   print('Nothing is implemented')
 
 
 def main():
+  init_argparser()
   init_db()
 
 
