abdcfc1a1609445f05ef88bd63d703ef9167b8fd
Anna Langner <annalangner06@gmail.com>
2020-09-03 12:37:33 +0200

    Builded a database

--- script/main.py
+++ script/main.py
@@ -1,5 +1,12 @@
 #A script that takes command and parameter as an argument
 import argparse
+import json 
+import sqlite3
+from sqlite3 import Error
+
+
+JSON_FILE = "init\persons.json"
+persons =  json.load(open(JSON_FILE, encoding='utf-8'))
 
 
 def init_argparser():
@@ -20,9 +27,56 @@ def init_argparser():
 
 
 def init_db():  
-  '''Initializing the database'''
+  '''Initializing the database'''  
+    
+  conn = None
+  try:
+    conn = sqlite3.connect("db/db_file.db")
+  except Error as e:
+    print(e)
 
-  print('Nothing is implemented')
+  cursor = conn.cursor()
+  cursor.execute('DROP TABLE IF EXISTS USERS')
+  sql_users_table = """ CREATE TABLE USERS (
+    GENDER,
+    NAME_TITLE,
+    NAME_FIRST CHAR(20) NOT NULL,
+    NAME_LAST CHAR(20),
+    LOCATION_STREET_NUMBER,
+    LOCATION_STREET_NAME,
+    LOCATION_CITY,
+    LOCATION_STATE,
+    LOCATION_COUNTRY,
+    LOCATION_POSTCODE,
+    LOCATION_COORDINATES_LATITUDE,
+    LOCATION_COORDINATES_LONGITUDE,
+    LOCATION_TIMEZONE_OFFSET,
+    LOCATION_TIMEZONE_DESCRIPTION,
+    EMAIL,
+    LOGIN_UUID,
+    LOGIN_USERNAME,
+    LOGIN_PASSWORD,
+    LOGIN_SALT,
+    LOGIN_MD5,
+    LOGIN_SHA1,
+    LOGIN_SHA256,
+    DOB_DATE,
+    DOB_AGE,
+    REGISTERED_DATE,
+    REGISTERED_AGE,
+    PHONE,
+    CELL,
+    ID_NAME,
+    ID_VALUE,
+    PICTURE_LARGE,
+    PICTURE_MEDIUM
+    PICTURE_THUMBNAIL,
+    NAT
+  ); """
+
+  cursor.execute(sql_users_table)
+  conn.commit()
+  conn.close()
 
 
 def percentage():
