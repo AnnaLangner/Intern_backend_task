a2538eff6307968dfd1bb3d047c6826a0cf84991
Anna Langner <annalangner06@gmail.com>
2020-09-21 11:15:51 +0200

    feature development

--- script/find_record.py
+++ script/find_record.py
@@ -3,15 +3,16 @@ import json
 
 JSON_FILE = "init\person-test.json"
 unflat_persons =  json.load(open(JSON_FILE, encoding='utf-8'))
+ 
 
+def print_user_name(first_name, last_name):
+  list_of_persons = unflat_persons['results']
+  for dict_of_person in list_of_persons:   
+    gender = dict_of_person['gender']
+    title = dict_of_person['name']['title']
+    first_name = dict_of_person['name']['first']
+    last_name = dict_of_person['name']['last']
+    print('first_name:' + first_name + '\n')
+    print('last_name: ' + last_name+ '\n')
 
-list_of_persons = unflat_persons['results']
-for dict_of_person in list_of_persons:   
-  gender = dict_of_person['gender']
-  title = dict_of_person['name']['title']
-  first_name = dict_of_person['name']['first']
-  last_name = dict_of_person['name']['last']
-  print(gender)
-  print(title)
-  print(first_name)
-  print(last_name)
+print_user_name('first', 'last')
