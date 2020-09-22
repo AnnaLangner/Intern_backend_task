4876ad4b42c905e2b45884ebdf0e9bf03fc6b290
Anna Langner <annalangner06@gmail.com>
2020-09-22 11:19:25 +0200

    Added print if user does not exist

--- script/find_record.py
+++ script/find_record.py
@@ -9,12 +9,17 @@ unflat_persons = json.load(open(JSON_FILE, encoding='utf-8'))
 
 def print_user_name(first_name):
   list_of_persons = unflat_persons['results']
+  record_found = False
   for dict_of_person in list_of_persons:   
     gender = dict_of_person['gender']
     title = dict_of_person['name']['title']
     first_name = dict_of_person['name']['first']
     last_name = dict_of_person['name']['last']
     if first_name == user_name:
-      print(first_name, last_name, gender)
+      record_found = True
+      print(first_name, last_name, gender)    
+  if record_found == False:
+    print('A user with this name does not exist')
+
 
 print_user_name(user_name)
