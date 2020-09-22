02b3231e5b263c3bd1696ad90e803d4b2d19aaad
Anna Langner <annalangner06@gmail.com>
2020-09-22 10:46:17 +0200

    Return list of records with the given name

--- script/find_record.py
+++ script/find_record.py
@@ -1,18 +1,20 @@
 import json 
+from sys import argv
 
+script, JSON_FILE, user_name  = argv
 
-JSON_FILE = "init\persons.json"
+#JSON_FILE = "init\persons.json"
 unflat_persons = json.load(open(JSON_FILE, encoding='utf-8'))
  
 
-def print_user_name(first_name, last_name):
+def print_user_name(first_name):
   list_of_persons = unflat_persons['results']
   for dict_of_person in list_of_persons:   
     gender = dict_of_person['gender']
     title = dict_of_person['name']['title']
     first_name = dict_of_person['name']['first']
     last_name = dict_of_person['name']['last']
-    print('first_name:' + first_name + '\n')
-    print('last_name: ' + last_name+ '\n')
+    if first_name == user_name:
+      print(first_name, last_name, gender)
 
-print_user_name('first', 'last')
+print_user_name(user_name)
