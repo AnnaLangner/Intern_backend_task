38763cc88a18fcf45756e9afa56e70b845fa20f0
Anna Langner <annalangner06@gmail.com>
2020-10-02 22:15:08 +0200

    separate functions

--- script/clear_the_phone_number.py
+++ script/clear_the_phone_number.py
@@ -2,26 +2,34 @@ import json
 import re
 
 
-JSON_FILE = 'init/persons.json'
-persons = json.load(open(JSON_FILE, encoding='utf-8'))
+persons = json.load(open('init/persons.json', encoding='utf-8'))
 
 
-def clear_the_phone():
-  persons_list = persons['results']
-  for persons_dict in persons_list:
-    phone = persons_dict['phone']       
+def find_record_with_picture():
+  record_names = []
+  dict_json_to_list = persons['results']
+  for dict_single_record in dict_json_to_list:
+    if 'phone' and 'cell' in dict_single_record:
+      record_names.append(dict_single_record)
+  return record_names
+
+
+def clear_the_phone(records):
+  for record in records:
+    phone = record['phone']       
     clear_phone = re.findall(r'[0-9]', phone)
     clear_phone = ''.join(clear_phone)
-    persons_dict['phone'] = clear_phone
+    record['phone'] = clear_phone
 
-    cell = persons_dict['cell'] 
+    cell = record['cell'] 
     clear_cell = re.findall(r'[0-9]', cell)
     clear_cell = ''.join(clear_cell)
-    persons_dict['cell'] = clear_cell
-
-  new_file = open(JSON_FILE, 'a')
-  new_file.close()
+    record['cell'] = clear_cell
 
 
-clear_the_phone()
-print(persons)
+def main():
+  records = find_record_with_picture()
+  clear_the_phone(records)
+  
+
+main()
