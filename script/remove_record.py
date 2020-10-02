ae28f2e67735c00306658051b47ae810bd3d776f
Anna Langner <annalangner06@gmail.com>
2020-10-02 20:39:53 +0200

    separate functions

--- script/remove_record.py
+++ script/remove_record.py
@@ -5,15 +5,25 @@ JSON_FILE = 'init/persons-test.json'
 persons = json.load(open(JSON_FILE, encoding='utf-8'))
 
 
-def remove_record_from_json():
+def find_record_with_picture():
+  record_names = []
   dict_json_to_list = persons['results']
   for dict_single_record in dict_json_to_list:
     if 'picture' in dict_single_record:
-      del dict_single_record['picture']
-
-  new_file = open(JSON_FILE, 'a')
-  new_file.close()
+      record_names.append(dict_single_record)
+  return record_names
 
 
-remove_record_from_json()
+def remove_record_from_json(records):
+  for record in records:
+    del record['picture']
+  
+
+
+def main():
+  records = find_record_with_picture()
+  remove_record_from_json(records)
+
+
+main()
 print(persons)
