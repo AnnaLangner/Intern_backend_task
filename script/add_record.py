ac772c88488c6cdedcfc4c15a607929d8ae41dfa
Anna Langner <annalangner06@gmail.com>
2020-10-02 21:55:27 +0200

    separate functions

--- script/add_record.py
+++ script/add_record.py
@@ -1,25 +1,36 @@
 import json
 from datetime import datetime
 
-JSON_FILE = 'init/persons-test.json'
-persons = json.load(open(JSON_FILE, encoding='utf-8'))
 
+persons = json.load(open('init/persons-test.json', encoding='utf-8'))
 
-def add_record_to_json():
+def find_record_with_dob():
+  record_names = []
   dict_json_to_list = persons['results']
   for dict_single_record in dict_json_to_list:
     if 'dob' in dict_single_record:
-      born = dict_single_record['dob']['date']
-      date_born = born[0:10]
-      date_born_dt = datetime.strptime(date_born, '%Y-%m-%d')
-      today = datetime.now()
-      time_until_birth = today - date_born_dt
-      dob_new_record = {"time_until_birth": str(time_until_birth)}
-      dict_single_record["dob"].update(dob_new_record)
-
-  new_file = open(JSON_FILE, 'a')
-  new_file.close()
+      record_names.append(dict_single_record)
+  return record_names
 
 
-add_record_to_json()
-print(persons)
+def create_new_record_in_dob(records):
+  for record in records:
+    days_left = 0
+    born = record['dob']['date']
+    date_born_dt = datetime.strptime(born, '%Y-%m-%dT%H:%M:%S.%fZ')
+    today = datetime.now().timetuple().tm_yday
+    birthday_day = date_born_dt.timetuple().tm_yday
+    if today > birthday_day:
+      days_left = (365-today)+birthday_day
+    else:
+      days_left = birthday_day - today    
+    dob_new_record = {"time_until_birthday": str(days_left)}
+    record["dob"].update(dob_new_record)
+ 
+
+def main():
+  records = find_record_with_dob()
+  create_new_record_in_dob(records)  
+
+
+main()
