dd1bbbc8578bf1dcbdb64d6264eb293e9a2c16c5
Anna Langner <annalangner06@gmail.com>
2020-10-08 12:58:38 +0200

    code review changes

--- script/find_record.py
+++ script/find_record.py
@@ -8,7 +8,7 @@ script, JSON_FILE, user_name  = argv
 unflat_persons = json.load(open(JSON_FILE, encoding='utf-8'))
  
 
-def find_user_by_name(first_name):
+def find_users_by_name(first_name):
   list_of_persons = unflat_persons['results']
   user_list = []
   for dict_of_person in list_of_persons:   
@@ -17,21 +17,20 @@ def find_user_by_name(first_name):
     first_name = dict_of_person['name']['first']
     last_name = dict_of_person['name']['last']
     if first_name == user_name:
-      record_found = True
       user_list.append((gender, title, first_name, last_name))
   return user_list
 
 
-def print_user_name(first_name):
-  print_user = find_user_by_name(user_name)
-  if print_user == []:
+def print_user_names(users):  
+  if users == []:
      print('A user with this name does not exist')
   else:
-    print(print_user)
+    print(users)
 
 
 def main():
-  print_user_name(user_name)
+  users = find_users_by_name(user_name)
+  print_user_names(users)
 
 
 main()
