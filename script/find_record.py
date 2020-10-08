ba4d243c7c6b708fa1d8e7568392cec99c76e093
Anna Langner <annalangner06@gmail.com>
2020-10-08 12:28:08 +0200

    separate functions

--- script/find_record.py
+++ script/find_record.py
@@ -5,13 +5,11 @@ from sys import argv
 script, JSON_FILE, user_name  = argv
 
 
-#JSON_FILE = "init\persons.json"
 unflat_persons = json.load(open(JSON_FILE, encoding='utf-8'))
  
 
-def find_user_name(first_name):
+def find_user_by_name(first_name):
   list_of_persons = unflat_persons['results']
-  record_found = False
   user_list = []
   for dict_of_person in list_of_persons:   
     gender = dict_of_person['gender']
@@ -21,14 +19,19 @@ def find_user_name(first_name):
     if first_name == user_name:
       record_found = True
       user_list.append((gender, title, first_name, last_name))
-  if record_found == False:
-    print('A user with this name does not exist')
-  return(user_list)
+  return user_list
 
 
 def print_user_name(first_name):
-  print_user = find_user_name(user_name)
-  print(print_user)
+  print_user = find_user_by_name(user_name)
+  if print_user == []:
+     print('A user with this name does not exist')
+  else:
+    print(print_user)
 
 
-print_user_name(user_name)
+def main():
+  print_user_name(user_name)
+
+
+main()
