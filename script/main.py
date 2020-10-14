47d5daa10a823ed90c73462fa219f86a684a30d0
Anna Langner <annalangner06@gmail.com>
2020-10-14 11:30:10 +0200

    The changes suggested in the PR were introduced

--- script/main.py
+++ script/main.py
@@ -8,85 +8,6 @@ JSON_FILE = "init\persons.json"
 unflat_people =  json.load(open(JSON_FILE, encoding='utf-8'))
 
 
-people = []
-
-list_of_people = unflat_people['results']
-for dict_of_person in list_of_people:   
-  gender = dict_of_person['gender']
-  name_title = dict_of_person['name']['title']
-  name_first = dict_of_person['name']['first']
-  name_last = dict_of_person['name']['last']
-  location_street_number = dict_of_person['location']['street']['number']
-  location_street_name = dict_of_person['location']['street']['name']
-  location_city = dict_of_person['location']['city']
-  location_state = dict_of_person['location']['state']
-  location_country = dict_of_person['location']['country']
-  location_postcode = dict_of_person['location']['postcode']
-  location_coordinates_latitude = dict_of_person['location']['coordinates']['latitude']
-  location_coordinates_longitude = dict_of_person['location']['coordinates']['longitude']
-  location_timezone_offset = dict_of_person['location']['timezone']['offset']
-  location_timezone_description = dict_of_person['location']['timezone']['description']
-  email = dict_of_person['email']
-  login_uuid = dict_of_person['login']['uuid']
-  login_username = dict_of_person['login']['username']
-  login_password = dict_of_person['login']['password']
-  login_salt = dict_of_person['login']['salt']
-  login_md5 = dict_of_person['login']['md5']
-  login_sha1 = dict_of_person['login']['sha1']
-  login_sha256 = dict_of_person['login']['sha256']
-  dob_date = dict_of_person['dob']['date']
-  dob_age = dict_of_person['dob']['age']
-  registered_date = dict_of_person['registered']['date']
-  registered_age = dict_of_person['registered']['age']
-  phone = dict_of_person['phone']
-  cell = dict_of_person['cell']
-  id_name = dict_of_person['id']['name']
-  id_value = dict_of_person['id']['value']
-  picture_large = dict_of_person['picture']['large']
-  picture_medium = dict_of_person['picture']['medium']
-  picture_thumbnail = dict_of_person['picture']['thumbnail']
-  nat = dict_of_person['nat']
-  columns = [
-    gender,
-    name_title,
-    name_first,
-    name_last,
-    location_street_number,
-    location_street_name,
-    location_city,
-    location_state,
-    location_country,
-    location_postcode,
-    location_coordinates_latitude,
-    location_coordinates_longitude,
-    location_timezone_offset,
-    location_timezone_description,
-    email,
-    login_uuid,
-    login_username,
-    login_password,
-    login_salt,
-    login_md5,
-    login_sha1,
-    login_sha256,
-    dob_date,
-    dob_age,
-    registered_date,
-    registered_age,
-    phone,
-    cell,
-    id_name,
-    id_value,
-    picture_large,
-    picture_medium,
-    picture_thumbnail,
-    nat
-  ]
-
-  people.append(columns)
-
-
-
 def init_argparser():
   """Fetch arguments from command line"""
 
@@ -124,16 +45,16 @@ def create_table(conn, create_table_sql):
 
 def insert_users_to_table(conn, users):
   sql = '''INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+
   cur = conn.cursor()
-  for columns in people:
+  for columns in users:
     cur.execute(sql, columns)  
   conn.commit()
 
 
-def init_db():  
+def init_db(conn):  
   '''Initializing the database'''  
 
-  database = "db/pythonsqliteusers.db"
   
   sql_create_users_table = ''' CREATE TABLE IF NOT EXISTS users (
     gender text,
@@ -170,9 +91,7 @@ def init_db():
     picture_medium text,
     picture_thumbnail text,
     nat
-  ); '''
-
-  conn = create_connection(database)  
+  ); '''  
 
   # create table
   if conn is not None:
@@ -180,8 +99,47 @@ def init_db():
   else:
     print("Error! cannot create the database connection.")
   
-  with conn:     
-    person = (
+
+def import_users_to_db(conn):
+  people = []
+
+  list_of_people = unflat_people['results']
+  for dict_of_person in list_of_people:   
+    gender = dict_of_person['gender']
+    name_title = dict_of_person['name']['title']
+    name_first = dict_of_person['name']['first']
+    name_last = dict_of_person['name']['last']
+    location_street_number = dict_of_person['location']['street']['number']
+    location_street_name = dict_of_person['location']['street']['name']
+    location_city = dict_of_person['location']['city']
+    location_state = dict_of_person['location']['state']
+    location_country = dict_of_person['location']['country']
+    location_postcode = dict_of_person['location']['postcode']
+    location_coordinates_latitude = dict_of_person['location']['coordinates']['latitude']
+    location_coordinates_longitude = dict_of_person['location']['coordinates']['longitude']
+    location_timezone_offset = dict_of_person['location']['timezone']['offset']
+    location_timezone_description = dict_of_person['location']['timezone']['description']
+    email = dict_of_person['email']
+    login_uuid = dict_of_person['login']['uuid']
+    login_username = dict_of_person['login']['username']
+    login_password = dict_of_person['login']['password']
+    login_salt = dict_of_person['login']['salt']
+    login_md5 = dict_of_person['login']['md5']
+    login_sha1 = dict_of_person['login']['sha1']
+    login_sha256 = dict_of_person['login']['sha256']
+    dob_date = dict_of_person['dob']['date']
+    dob_age = dict_of_person['dob']['age']
+    registered_date = dict_of_person['registered']['date']
+    registered_age = dict_of_person['registered']['age']
+    phone = dict_of_person['phone']
+    cell = dict_of_person['cell']
+    id_name = dict_of_person['id']['name']
+    id_value = dict_of_person['id']['value']
+    picture_large = dict_of_person['picture']['large']
+    picture_medium = dict_of_person['picture']['medium']
+    picture_thumbnail = dict_of_person['picture']['thumbnail']
+    nat = dict_of_person['nat']
+    columns = [
       gender,
       name_title,
       name_first,
@@ -216,8 +174,11 @@ def init_db():
       picture_medium,
       picture_thumbnail,
       nat
-    )
-    user_id = insert_users_to_table(conn, person)
+    ]
+
+    people.append(columns)      
+    
+  insert_users_to_table(conn, people)
 
 
 def percentage():
@@ -226,9 +187,11 @@ def percentage():
 
 def main():
   results = []
+  conn = create_connection('db/pythonsqliteusers.db')
   args = init_argparser()
   if args.operation == 'init':
-    results = init_db()
+    init_db(conn)
+    results = import_users_to_db(conn)
   elif args.operation == 'percentage':
     results = percentage()
 
