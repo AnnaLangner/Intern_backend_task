8e140566bd03233d753aeedd5f0dc525a4bfb75f
Anna Langner <annalangner06@gmail.com>
2020-11-22 17:08:38 +0100

    Correction of typos

--- script/main_test.py
+++ script/main_test.py
@@ -6,23 +6,23 @@ class TestRemovesSpecialCharacters(unittest.TestCase):
 
   def test_remove_special_characters(self):
 
-    self.assertEqual(main.clear_phone_numbers("04-56-18-88-34"),"0456188834")
-    self.assertEqual(main.clear_phone_numbers("06-74-93-14-75"),"0674931475") 
+    self.assertEqual(main.clear_phone_number("04-56-18-88-34"),"0456188834")
+    self.assertEqual(main.clear_phone_number("06-74-93-14-75"),"0674931475") 
 
-    self.assertEqual(main.clear_phone_numbers("075 831 68 55"),"0758316855")
-    self.assertEqual(main.clear_phone_numbers("075 292 22 27"),"0752922227")
+    self.assertEqual(main.clear_phone_number("075 831 68 55"),"0758316855")
+    self.assertEqual(main.clear_phone_number("075 292 22 27"),"0752922227")
 
-    self.assertEqual(main.clear_phone_numbers("(394)-381-6746"),"3943816746")
-    self.assertEqual(main.clear_phone_numbers("(361)-356-8993"),"3613568993")
+    self.assertEqual(main.clear_phone_number("(394)-381-6746"),"3943816746")
+    self.assertEqual(main.clear_phone_number("(361)-356-8993"),"3613568993")
 
-    self.assertEqual(main.clear_phone_numbers("(00) 1170-0136"),"0011700136")
-    self.assertEqual(main.clear_phone_numbers("(32) 7367-6536"),"3273676536")
+    self.assertEqual(main.clear_phone_number("(00) 1170-0136"),"0011700136")
+    self.assertEqual(main.clear_phone_number("(32) 7367-6536"),"3273676536")
 
 
   def test_are_no_special_characters(self):
 
-    self.assertEqual(main.clear_phone_numbers("0456188834"),"0456188834")
-    self.assertEqual(main.clear_phone_numbers("0674931475"),"0674931475") 
+    self.assertEqual(main.clear_phone_number("0456188834"),"0456188834")
+    self.assertEqual(main.clear_phone_number("0674931475"),"0674931475") 
 
 
 class TestIsLeapYear(unittest.TestCase):
@@ -36,6 +36,7 @@ class TestIsLeapYear(unittest.TestCase):
 
   def test_is_not_leap_year(self):
     self.assertFalse(main.is_leap_year(100), True)
+    self.assertFalse(main.is_leap_year(2007), True)
 
 if __name__ == '__main__':
     unittest.main()
